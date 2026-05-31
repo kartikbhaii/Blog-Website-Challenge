5575948f872d66120686826005beb0ba53e35d56
Kartikey_Singh <kkartikeysinghh@gmail.com>
2026-05-31 20:24:51 +0530

    implemented mongoDB using mongoose so our blogs can be stored in a database

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+const dns = require("dns");
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://kk:kkhannu0@cluster0.piqnmt9.mongodb.net/blogDB?retryWrites=true&w=majority&appName=Cluster0")
+  .then(() => {
+    console.log("Connected to MongoDB successfully.");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+})
+
+const Post = mongoose.model("Post", postSchema)
 
 let homeStartingContent =
   "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat nulla ipsa laborum quasi iure totam autem veritatis aut, assumenda nesciunt ullam maiores necessitatibus cumque dicta odio saepe? In mollitia fuga rerum explicabo perspiciatis ex vel unde eveniet fugit cupiditate doloribus enim eos quibusdam repellat esse, asperiores ullam iure accusamus itaque recusandae illo animi nisi. Veritatis quasi reiciendis cupiditate dicta aspernatur!";
@@ -17,13 +36,17 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let posts = [];
 
 app.get("/", (req, res) => {
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts,
-
+  Post.find({})
+  .then((posts) => {
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+    });
+  })
+  .catch((err) => {
+    console.log(err);
   });
 });
 
@@ -43,13 +66,36 @@ app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
+app.get("/posts/:postId", (req, res)=>{
+const requestedPostId = req.params.postId;
+Post.findOne({ _id: requestedPostId })
+  .then((post) => {
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+
+})
+
 app.post("/compose", (req, res) => {
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody,
-  };
-  posts.push(post);
-  res.redirect("/");
+  })
+  post.save()
+  .then(() => {
+    res.redirect("/");
+  })
+  .catch((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
 });
 
 
